07664c66a8e6dbc94c34e582fa9ac6d987a027d2
Sebastian <95152236+sebferreira@users.noreply.github.com>
2024-06-04 23:42:32 -0300

    segundo commit

---
+++ nodeJS-2/db.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql');
+const { promisify } = require('util');
+
+const db = mysql.createPool({
+    host: "127.0.0.1",
+    user:"root",
+    password:"root",
+    database:"usuarios_db",
+})
+
+const promiseQuery = promisify(db.query).bind(db)
+
+db.getConnection((err,con)=>{
+    try {
+        con.query('CREATE TABLE IF NOT EXISTS usuarios (id_usuario INT NOT NULL AUTO_INCREMENT, nombre VARCHAR(50) NOT NULL, apellido VARCHAR(50) NOT NULL, edad INT NOT NULL, PRIMARY KEY(id_usuario))')
+    } catch (error) {
+        throw err;
+    }
+})
+
+module.exports = promiseQuery
